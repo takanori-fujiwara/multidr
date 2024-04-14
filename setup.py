a0624d7da28bd79e775e22cfafc1a549c4d75bf3
Takanori Fujiwara <taka.fuji.fujiwara@gmail.com>
2024-04-14 20:59:02 +0200

    sklearn to scikit-learn

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ import os
 from distutils.core import setup
 
 setup(
-    name='multidr',
+    name="multidr",
     version=0.04,
-    packages=[''],
-    package_dir={'': '.'},
-    install_requires=['scipy', 'numpy', 'sklearn', 'umap-learn', 'matplotlib'],
-    py_modules=['multidr', 'multidr.tdr', 'multidr.cl'])
+    packages=[""],
+    package_dir={"": "."},
+    install_requires=["scipy", "numpy", "scikit-learn", "umap-learn", "matplotlib"],
+    py_modules=["multidr", "multidr.tdr", "multidr.cl"],
+)
